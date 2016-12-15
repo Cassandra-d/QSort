5e6f00ab83fda440605e369ce3e5f33f6b15d2c8
CassandraDied <cassandradied@gmail.com>
2016-12-16 00:59:47 +0300

    Added new algo, added some perf and correctness tests, adjusted initial algo

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace QSort_Naive
 {
     class Program
     {
+        static void Swap(int[] arr, int i, int j)
+        {
+            int tmp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = tmp;
+        }
+
         static void SwapIf(int[] arr, int i, int j)
         {
             var k = arr[j];
@@ -14,7 +23,87 @@ namespace QSort_Naive
             }
         }
 
+        static bool SwapIf2(int[] arr, int i, int j)
+        {
+            var k = arr[j];
+            if (arr[i] > arr[j])
+            {
+                arr[j] = arr[i];
+                arr[i] = k;
+                return true;
+            }
+            return false;
+        }
+
         static void QSort(int[] arr, int lo, int hi)
+        {
+            if (hi - lo == 0)
+                return;
+            if (hi - lo == 1)
+            {
+                SwapIf(arr, lo, hi);
+                return;
+            }
+
+            int pivot = arr[(hi + lo) / 2];
+            int i = lo;
+            int j = hi;
+
+            while (j > i)
+            {
+                if (arr[j] > pivot)
+                {
+                    --j;
+                    continue;
+                }
+
+                while (i < j && arr[i] < pivot)
+                {
+                    ++i;
+                }
+
+                if (i < j)
+                {
+                    Swap(arr, i, j);
+                    --j;
+                }
+            }
+
+            QSort(arr, lo, i);
+            QSort(arr, i + 1, hi);
+        }
+
+        // from https://visualgo.net/sorting
+        static void QSort2(int[] arr, int lo, int hi)
+        {
+            if (hi - lo == 1)
+            {
+                SwapIf(arr, lo, hi);
+                return;
+            }
+
+            int pivot = arr[lo];
+            int storeIndex = lo + 1;
+            int k = storeIndex;
+            while (k <= hi)
+            {
+                if (arr[k] < pivot)
+                {
+                    if (k != storeIndex)
+                        Swap(arr, k, storeIndex);
+                    storeIndex += 1;
+                }
+                k += 1;
+            }
+            var wasSwap = SwapIf2(arr, lo, storeIndex - 1);
+
+            if (storeIndex - 2 > lo)
+                QSort2(arr, wasSwap ? lo : lo + 1, storeIndex - 2);
+            if (storeIndex < hi)
+                QSort2(arr, storeIndex, hi);
+        }
+
+        static void QSortParallel(int[] arr, int lo, int hi, int lvl = 0)
         {
             if (hi - lo == 0)
                 return;
@@ -52,19 +141,40 @@ namespace QSort_Naive
                 }
             }
 
-            QSort(arr, lo, i);
-            QSort(arr, i + 1, hi);
+            if (lvl > 4)
+            {
+                var tasks = new[]
+                {
+                    Task.Run(() => QSort(arr, lo, i)),
+                    Task.Run(() => QSort(arr, i + 1, hi))
+                };
+                Task.WaitAll(tasks);
+            }
+            else
+            {
+                QSortParallel(arr, lo, i, lvl + 1);
+                QSortParallel(arr, i + 1, hi, lvl + 1);
+            }
         }
 
         static void Main(string[] args)
         {
+            int cnt = 10000000;
             var arrays = new[]
             {
+                new[] {1},
+                new[] {1,2},
+                new[] {2,1},
+                new[] {2, 2, 3, 5, 3, 5},
                 new[] { 3, 2, 4, 5, 1 },
+                new[] { 3, 2, 5, 4, 1 },
+                new[] { 3, 2, 5, 4, 1, 0 },
+                new[] { 3, 2, 5, 4, 1, 1 },
                 new[] { 1, 2, 3, 6, 5 },
-                new[] { 6, 5, 4, 3, 2, 1 },
-                new[] { 1, 2, 4, 5, 6, 3, 4, 5, 6, 6 },
                 new[] { 1, 2, 3, 4, 5, 6 },
+                new[] { 6, 5, 4, 3, 2, 1},
+                new[] { 1, 2, 4, 5, 6, 3, 4, 5, 6, 6 },
+                new[] { 1, 2, 4, 5, 6, 1, 2, 3, 4, 5, 6 },
                 new[] { 1, 2, 4, 5, 6, 3, 3},
                 new[] { 3, 3, 6, 5, 4, 2, 1 },
                 new[] { 3, 3, 7, 1, 1 },
@@ -72,13 +182,10 @@ namespace QSort_Naive
                 new[] { 1, 1, 7, 7, 3, 3 },
                 new[] { 3, 3, 7, 7, 1, 1 },
                 new[] { 1, 7, 7, 7, 3 },
-                new[] {1,2,3,4,5,6},
-                new[] {6,5,4,3,2,1},
-                new[] {1,2,3,4,5},
-                new[] {5,4,3,2,1},
+                new[] { 1, 2, 3, 4, 5},
+                new[] { 5, 4, 3},
                 new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
-                new[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1 },
-                new[] {42, 9, 17, 54, 602, -3, 54, 999, -11 },
+                new[] { 42, 9, 17, 54, 602, -3, 54, 999, -11 },
                 new[] {-11, -3, 9, 17, 42, 54, 54, 602, 999 },
                 new[] {1,1,1,1,1,1,1,1,1,1,0},
                 new[] {1,1,1,1,1,1,1,1,1,0,0},
@@ -86,11 +193,19 @@ namespace QSort_Naive
                 new[] {9,1,1,1,1,1,1,1,1,1,1,1},
                 new[] {9,9,1,1,1,1,1,1,1,1,1,1},
                 new[] {9,9,9,1,1,1,1,1,1,1,1,1},
-                GetRandomArray(10000000),
+                GetRandomArray(cnt),
+                GetRandomArray(cnt),
+                GetRandomArray(cnt),
+                GetRandomArray(cnt),
+                GetRandomArray(cnt),
+                GetRandomArray(cnt),
+                GetRandomArray(cnt),
+                GetRandomArray(cnt),
+                GetRandomArray(cnt),
             };
-
             long overallMS = 0;
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            Console.WriteLine("Go");
             foreach (var arr in arrays)
             {
                 sw.Restart();
